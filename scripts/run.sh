c2c73f80c37027110b4fb936339e329d586d2fbd
KyBarrioga <kybarrioga@gmail.com>
2026-04-01 10:10:39 -0700

    fix: updated uWSGI buffer limit

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+UWSGI_BUFFER_SIZE="${UWSGI_BUFFER_SIZE:-65535}"
+
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 
-uwsgi --http :8000 --master --enable-threads --module app.wsgi
+uwsgi \
+  --http :8000 \
+  --master \
+  --enable-threads \
+  --buffer-size "${UWSGI_BUFFER_SIZE}" \
+  --module app.wsgi
